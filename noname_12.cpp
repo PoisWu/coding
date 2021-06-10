0d930a93f15139593daff6f9e839a4183ef18e33
cheng-yen.wu <cheng-yen.wu@cheng-yenwus-MacBook-Pro.local>
2021-06-10 10:18:57 +0200

    rename

---
+++ noname_12.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+
+using namespace std;
+
+class Solution{
+    public:
+        int hello(){
+            cout<< "hello" <<endl;
+        }      
+};
+
+int main(){
+    cout << fine; 
+    return 0;
+}
