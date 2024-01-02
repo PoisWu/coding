a24995c75344e60bb52ed4682a080d59a374e5bd
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-01-02 19:21:49 +0800

    Add c++ template files

---
+++ template/template.cpp
@@ -0,0 +1,11 @@
+# include <vector>
+# include <algorithm>    
+# include <iostream>
+
+using namespace std;
+
+class Solution{
+public: 
+    
+};
+
---
+++ template/test.cpp
@@ -0,0 +1,18 @@
+# include <iostream> 
+# include <vector>
+using namespace std;
+template<typename T>
+void print_2d_vector(vector<vector<T> > vvec){
+    for (auto vec: vvec) {
+        for (auto e: vec){
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    Solution solver;
+    cout << "start" << endl;
+    
+}
