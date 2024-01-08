e3f30843d0c194a33f1ba057ace77d06dfe1bcb4
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-01-08 09:42:48 +0800

    Solution.cpp format

--- template/solution.cpp
+++ template/solution.cpp
@@ -3,6 +3,17 @@
 #include <map>
 #include <vector>
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right)
+    {
+    }
+}
 
 using namespace std;
 
