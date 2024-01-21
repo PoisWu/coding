bab601a0416afde7474d5cd7ec71ba50a3d96f8c
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-01-21 20:22:09 +0800

    Attempt to solve 3017

--- 3017. Count the Number of Houses at a Certain Distance II/solution.cpp
+++ 3017. Count the Number of Houses at a Certain Distance II/solution.cpp
@@ -40,54 +40,45 @@ public:
     {
         x = x - 1;
         y = y - 1;
-        vector<vector<long long>> dist(n, vector<long long>(n, 1000000 * n));
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                dist[j][i] = j - i;
-                dist[i][j] = j - i;
-            }
-        }
-
         if (x > y) {
             y = x ^ y;
             x = x ^ y;
             y = x ^ y;
         }
-        dist[x][y] = 1;
-        dist[y][x] = 1;
-        print_2d_vector(dist);
-        if (y - x) {
-            for (int k = y + x / 2; k < n; k++) {
-                if (dist[x][k] > dist[x][y] + dist[y][k]) {
-                    dist[x][k] = dist[x][y] + dist[y][k];
-                    dist[k][x] = dist[k][y] + dist[y][x];
-                }
-            }
-            for (int k = y + x / 2; k >= 0; k--) {
-                if (dist[y][k] > dist[y][x] + dist[x][k]) {
-                    dist[y][k] = dist[x][y] + dist[x][k];
-                    dist[k][y] = dist[k][y] + dist[x][x];
-                }
-            }
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (dist[i][j] > dist[i][x] + dist[x][j]) {
-                        dist[i][j] = dist[i][x] + dist[x][j];
-                    }
-                    if (dist[i][j] > dist[i][y] + dist[y][j]) {
-                        dist[i][j] = dist[i][y] + dist[y][j];
-                    }
-                }
-            }
+        vector<long long> res(n, 0);
+
+        // Compute in the circle
+        int dxy = y - x;
+        for (int i = 0; i < dxy / 2; i++) {
+            res[i] += (dxy + 1) * 2
+        }
+        if (dxy % 2) {
+            // dxy is odd
+            res[dxy / 2 + 1] += dxy + 1
+        }
+        // Compute L
+        int dlx = x;
+        for (int i = 0; i < dlx; i++) {
+            res[i] += (dlx - 1 - i) * 2
         }
 
-        vector<long long> res(n, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                res[dist[i][j] - 1] += 2;
+        // Compute R
+        int dyr = n - y - 1;
+        for (int i = 0; i < dyr; i++) {
+            res[i] += (dyr - 1 - i) * 2;
+        }
+
+        // compute L/R
+        if (dxy) {
+            // x != y
+            for (int i = 3; 1 + dlx + dyr; i++) {
+                res[i] += min(i - 2, 1 + dlx + dyr - i + 1);
+            }
+        } else {
+            for (int i = 2; dlx + dyr; i++) {
+                res[i] += min(i - 1, dlx + dyr - i + 1);
             }
         }
-        return res;
+        // compute L/C
     }
 };
