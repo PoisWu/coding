c3191d9eafadd457529fbb3a4858329cccf597f8
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-01-21 16:39:26 +0800

    daily challenge 0198

--- 0198. House Robber/solution.cpp
+++ 0198. House Robber/solution.cpp
@@ -26,7 +26,23 @@ class Solution
 public:
     int rob(vector<int> &nums)
     {
-        array<int, nums.size()> dp;
-        // dp[i] store how much money can still from
+        int n = nums.size();
+        if (n == 1)
+            return nums[0];
+        int dp[n];
+        // dp[i] store how much money can still from [0,i] must pick nums[i]
+        dp[0] = nums[0];
+        for (long unsigned int i = 1; i < nums.size(); i++) {
+            if (i == 1) {
+                dp[i] = nums[1];
+            }
+            if (i == 2) {
+                dp[i] = dp[i - 2] + nums[i];
+            }
+            if (i >= 3) {
+                dp[i] = max(dp[i - 2], dp[i - 3]) + nums[i];
+            }
+        }
+        return max(dp[n - 1], dp[n - 2]);
     }
 };
--- 0198. House Robber/test.cpp
+++ 0198. House Robber/test.cpp
@@ -17,4 +17,8 @@ int main()
 {
     Solution solver;
     cout << "start" << endl;
+    vector<int> v1 = {1, 2, 3, 1};
+    vector<int> v2 = {2, 7, 9, 3, 1};
+    cout << solver.rob(v1) << endl;
+    cout << solver.rob(v2) << endl;
 }
