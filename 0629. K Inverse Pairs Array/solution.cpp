192368e926cec60cb6f82a2fed15e45a16ba76b6
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-01-27 22:50:49 +0800

    daily challenge 0629

---
+++ 0629. K Inverse Pairs Array/solution.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right)
+    {
+    }
+};
+
+using namespace std;
+
+template <typename T>
+void print_2d_vector(vector<vector<T>> vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+
+class Solution
+{
+private:
+    static const int mod = 1000000007;
+
+public:
+    int kInversePairs(int n, int k)
+    {
+        // 0 - k pair permutations
+        vector<vector<int>> dp(n + 1, vector<int>(k + 1, 0));
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= k; j++) {
+                dp[i][j] = (dp[i - 1][j] + (j > 0 ? dp[i][j - 1] : 0)) % mod;
+                dp[i][j] =
+                    (dp[i][j] + mod - (i >= j ? dp[i - 1][j - i] : 0)) % mod;
+            }
+        }
+        return dp[n][k];
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector < ? ? > v1 = {};
+    string s1 = "";
+    string t1 = "";
+
+    // Following tree
+    //              - Null
+    //       - 5 - |
+    //      |      |      - 9
+    //  1 - |       - 4 -|
+    //      |             - 2
+    //      |       - 10
+    //       - 3 - |
+    //              - 6
+    //
+    // TreeNode two = TreeNode(2);
+    // TreeNode nine = TreeNode(9);
+    // TreeNode four = TreeNode(4, &nine, &two);
+    // TreeNode five = TreeNode(5, NULL, &four);
+    // TreeNode ten = TreeNode(10);
+    // TreeNode six = TreeNode(6);
+    // TreeNode three = TreeNode(3, &ten, &six);
+    // TreeNode one = TreeNode(1, &five, &three);
+}
