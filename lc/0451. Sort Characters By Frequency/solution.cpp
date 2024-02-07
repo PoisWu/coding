cc1f5df76bb1ddf673fc2e25513c3ca5ad6067ee
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-02-07 22:42:07 +0800

    daily challenge 0451

---
+++ lc/0451. Sort Characters By Frequency/solution.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right)
+    {
+    }
+};
+
+using namespace std;
+
+template <typename T>
+void print_2d_vector(vector<vector<T>> vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution
+{
+private:
+    static const int mod = 1000000007;
+
+public:
+    string frequencySort(string s)
+    {
+        unordered_map<char, int> map;
+        for (int i = 0; i < s.length(); i++) {
+            map[s[i]]++;
+        }
+        vector<pair<int, char>> freq;
+
+        for (auto &it : map) {
+            freq.push_back(make_pair(it.second, it.first));
+        }
+        sort(freq.begin(), freq.end());
+        string res;
+        for (int i = freq.size() - 1; i >= 0; i--) {
+            res += string(freq[i].first, freq[i].second);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "tree";
+    cout << solver.frequencySort(s1) << endl;
+    return 0;
+
+    // Following tree
+    //              - Null
+    //       - 5 - |
+    //      |      |      - 9
+    //  1 - |       - 4 -|
+    //      |             - 2
+    //      |       - 10
+    //       - 3 - |
+    //              - 6
+    //
+    // TreeNode two = TreeNode(2);
+    // TreeNode nine = TreeNode(9);
+    // TreeNode four = TreeNode(4, &nine, &two);
+    // TreeNode five = TreeNode(5, NULL, &four);
+    // TreeNode ten = TreeNode(10);
+    // TreeNode six = TreeNode(6);
+    // TreeNode three = TreeNode(3, &ten, &six);
+    // TreeNode one = TreeNode(1, &five, &three);
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T>> vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
