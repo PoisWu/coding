108fe6a2ba03631c31fc6b151b847bf1f39f06ca
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-02-14 09:31:51 +0800

    Dailly challenge 2149

---
+++ lc/2149. Rearrange Array Elements by Sign/solution.cpp
@@ -0,0 +1,106 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right)
+    {
+    }
+};
+
+using namespace std;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution
+{
+private:
+    static const int mod = 1000000007;
+
+public:
+    vector<int> rearrangeArray(vector<int> &nums)
+    {
+        vector<int> pos;
+        vector<int> neg;
+        for (int x : nums) {
+            if (x > 0) {
+                pos.push_back(x);
+            } else {
+                neg.push_back(x);
+            }
+        }
+        vector<int> res;
+        for (int i = 0; i < pos.size(); i++) {
+            res.push_back(pos[i]);
+            res.push_back(neg[i]);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+
+    // Following tree
+    //              - Null
+    //       - 5 - |
+    //      |      |      - 9
+    //  1 - |       - 4 -|
+    //      |             - 2
+    //      |       - 10
+    //       - 3 - |
+    //              - 6
+    //
+    // TreeNode two = TreeNode(2);
+    // TreeNode nine = TreeNode(9);
+    // TreeNode four = TreeNode(4, &nine, &two);
+    // TreeNode five = TreeNode(5, NULL, &four);
+    // TreeNode ten = TreeNode(10);
+    // TreeNode six = TreeNode(6);
+    // TreeNode three = TreeNode(3, &ten, &six);
+    // TreeNode one = TreeNode(1, &five, &three);
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
