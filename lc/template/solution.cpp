41b76cd2425c737d92cc7d2dc13f2cbcf8f40d6a
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-02-18 20:42:43 +0800

    Update template

--- lc/template/solution.cpp
+++ lc/template/solution.cpp
@@ -4,25 +4,17 @@
 #include <iostream>
 #include <map>
 #include <queue>
+#include <set>
 #include <stack>
 #include <string>
+#include <tuple>
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right)
-        : val(x), left(left), right(right)
-    {
-    }
-};
 
 using namespace std;
+using lli = long long int;
 
 template <typename T>
 void print_2d_vector(vector<vector<T> > vvec);
@@ -46,25 +38,6 @@ int main()
     cout << solver.<< endl;
     print_vector(solver.);
     return 0;
-
-    // Following tree
-    //              - Null
-    //       - 5 - |
-    //      |      |      - 9
-    //  1 - |       - 4 -|
-    //      |             - 2
-    //      |       - 10
-    //       - 3 - |
-    //              - 6
-    //
-    // TreeNode two = TreeNode(2);
-    // TreeNode nine = TreeNode(9);
-    // TreeNode four = TreeNode(4, &nine, &two);
-    // TreeNode five = TreeNode(5, NULL, &four);
-    // TreeNode ten = TreeNode(10);
-    // TreeNode six = TreeNode(6);
-    // TreeNode three = TreeNode(3, &ten, &six);
-    // TreeNode one = TreeNode(1, &five, &three);
 }
 
 template <typename T>
