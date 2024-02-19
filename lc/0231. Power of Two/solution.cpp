5e7c256e7c543005bad1da89924bcb5d86546c7f
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-02-19 09:36:35 +0800

    dailly challenge 0231

---
+++ lc/0231. Power of Two/solution.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+using lli = long long int;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution
+{
+private:
+    static const int mod = 1000000007;
+
+public:
+    bool isPowerOfTow(int n)
+    {
+        if (n <= 0) {
+            return false;
+        }
+        return !n & (n - 1);
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
