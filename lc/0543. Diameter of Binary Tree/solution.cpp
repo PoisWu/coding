f38507e22c6e568601fde3f69456ac33cf3a9e3b
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-02-27 20:49:27 +0800

    Daily challenge 0543

---
+++ lc/0543. Diameter of Binary Tree/solution.cpp
@@ -0,0 +1,81 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+using lli = long long int;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution
+{
+private:
+    static const int mod = 1000000007;
+
+    int hight(TreeNode *root, int *diameter)
+    {
+        if (!root) {
+            return -1;
+        }
+        int h_left = hight(root->left);
+        int h_right = hight(root->right);
+        *diameter = max(*diameter, h_left + h_right + 2);
+        return 1 + max(h_left, h_right);
+    }
+
+
+public:
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        int diameter = 0;
+
+        hight(root, &diameter);
+        return diameter;
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
