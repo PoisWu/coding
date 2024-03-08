0d97fa6df9ca0f3116cec84bc95c6d204a3edcb3
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-03-08 22:19:16 +0800

    dailly challenge

---
+++ lc/3005. Count Elements With Maximum Frequency/solution.cpp
@@ -0,0 +1,77 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+using lli = long long int;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution
+{
+public:
+    int maxFrequencyElements(vector<int> &nums)
+    {
+        int freq[101] = {0};
+        for (int x : nums) {
+            freq[x]++;
+        }
+        int max_freq = 0;
+        int res = 0;
+        for (int i = 0; i < 101; i++) {
+            if (freq[i] > max_freq) {
+                max_freq = freq[i];
+                res = 1;
+            } else if (freq[i] == max_freq) {
+                res++;
+            }
+        }
+        return res * max_freq;
+    }
+};
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
