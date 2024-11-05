fe94036e9ed0f596fdc1a4e15579ea52c600e8da
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-11-05 17:31:39 +0800

    Daily update (Tue 05 Nov)

---
+++ lc/2914. Minimum Number of Changes to Make Binary String Beautiful/solution.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+using lli = long long int;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution {
+public:
+    int minChanges(string s) {
+        int ret = 0;
+        int n = s.length();
+        for (int i=0; i<n;i=i+2){
+            if(s[i]!=s[i+1])
+                ret++;
+        }
+        return ret;
+    }
+};
+
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
