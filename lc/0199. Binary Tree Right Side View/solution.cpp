2041956990efa4a3007f60ae3b2de53db2cb896a
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2025-01-09 19:21:08 +0800

    daily update

---
+++ lc/0199. Binary Tree Right Side View/solution.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+using lli = long long int;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        // Using BFS
+        queue<TreeNode*> fifo;
+        vector<int> ret;
+        if (!root){
+            return ret;
+        }
+        fifo.push(root);
+    
+
+        while (!fifo.empty()){
+            int nb = fifo.size();
+            for (int i = 0 ; i < nb; i++){
+                TreeNode* node = fifo.front();
+                fifo.pop();
+                if (node->left) 
+                    fifo.push(node->left);
+                if (node->right) 
+                    fifo.push(node->right);
+                if (i == nb -1){
+                    ret.push_back(node->val);
+                }
+            }
+        }
+        return ret;
+
+    }
+};
+
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
---
+++ lc/2185. Counting Words With a Given Prefix/solution.cpp
@@ -0,0 +1,75 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+using namespace std;
+using lli = long long int;
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec);
+template <typename T>
+void print_vector(vector<T> vec);
+
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int ret = 0;
+        int n = pref.length();
+        for (string s: words){
+            for (int i = 0; i<n; i++){
+                if (s[i]!=pref[i]){
+                    break;
+                }
+                if (i == n-1){
+                    ret++;
+                }
+            }
+        }
+        return ret;
+        
+    }
+};
+
+
+int main()
+{
+    Solution solver;
+    vector<int> v1 = {};
+    string s1 = "";
+    string t1 = "";
+    cout << solver.<< endl;
+    print_vector(solver.);
+    return 0;
+}
+
+template <typename T>
+void print_vector(vector<T> vec)
+{
+    for (auto e : vec) {
+        cout << e << " ";
+    }
+    cout << endl;
+}
+
+template <typename T>
+void print_2d_vector(vector<vector<T> > vvec)
+{
+    for (auto vec : vvec) {
+        for (auto e : vec) {
+            cout << e << " ";
+        }
+        cout << endl;
+    }
+}
